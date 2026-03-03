d803eaa4ece0441acf596d480ba6432a4db1d7ac
Tebogo <notorioustee11@gmail.com>
2026-03-03 15:34:12 +0200

    Resolved Database Error Created UserRepository class to allow Users Table to talk to it

---
+++ src/main/java/za/ac/cput/unitrade/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package za.ac.cput.unitrade.repository;
+
+import za.ac.cput.unitrade.dao.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUniversityEmail(String universityEmail);
+
+    boolean existsByUniversityEmail(String universityEmail);
+}
